25a24fef170eec27d8a61a33d5ed73021521ad01
alphasave1 <akimitu9717@gmail.com>
2017-11-05 22:38:11 +0900

    Update modsListApiSettings.py

--- modsListApiSettings.py
+++ modsListApiSettings.py
@@ -2,4 +2,4 @@ from gui.app_loader.loader import g_appLoader
 from gui.modsListApi import g_modsListApi
 mod_name='mod_OfflineTest'
 def init():
-  g_modsListApi.addModification(id='mod_offline',name=mod_name,description=mod_name,enabled=True,callback=''login=True,lobby=False,icon='')
+  g_modsListApi.addModification(id='mod_offline',name=mod_name,description=mod_name,enabled=True,callback='',login=True,lobby=False,icon='')
